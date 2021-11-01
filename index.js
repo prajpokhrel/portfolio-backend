78f84bc40052cb33f78b1927e4a41b069a2daf8b
Prajwal Pokhrel <prajwal.pokhrel18@gmail.com>
2021-11-01 13:27:53 +0545

    cors refactoring

--- index.js
+++ index.js
@@ -39,17 +39,19 @@ mongoose.connect(portfolioDB)
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+// app.use(cors());
+app.use(cors({credentials: true, origin: 'https://portfolio-builder.prajwalp.com.np/'}));
 
 app.use('/assets/images', express.static(path.join('assets', 'images')));
 
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.setHeader("Access-Control-Allow-Origin", "https://portfolio-builder.prajwalp.com.np/");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    next();
-});
+// app.use(function (req, res, next) {
+//     //Enabling CORS
+//     res.setHeader("Access-Control-Allow-Origin", "https://portfolio-builder.prajwalp.com.np/");
+//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+//     res.setHeader("Access-Control-Allow-Credentials", "include");
+//     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+//     next();
+// });
 
 app.use('/api/v1/auth', auth);
 app.use('/api/v1/users', users);
